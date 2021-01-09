c29aa29c73212cb1c5e81f399059828e90b4d79c
prana <prana@LAPTOP-2KM7DM1I>
2021-01-09 15:06:32 +0530

    First change in Tables.java

--- Project1/src/day1/Tables.java
+++ Project1/src/day1/Tables.java
@@ -6,14 +6,15 @@ public class Tables {
 	
 	public static void main(String[] args) {
 		
-		int a;
+		int a,c;
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the number whose table you want to Print");
-		
-		a=s.nextInt();
+				a=s.nextInt();
+				System.out.println("Till what you want to print table");
+				c=s.nextInt();
 		System.out.println("Table of " + a);
 		System.out.println();
-		for(int i=1;i<=10;i++)
+		for(int i=1;i<=c;i++)
 		{
 			int b=a*i;
 			System.out.println(a+"x"+i+"="+b);
